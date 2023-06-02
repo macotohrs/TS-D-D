8627a6f3702b9e8cebf0124a3bdae56c6cbf2318
macotonacamura <65491593+macotonacamura@users.noreply.github.com>
2023-06-02 12:51:12 +0900

    124 DOM要素の操作

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,9 @@ class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
   element: HTMLFormElement;
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  mandayInputElement: HTMLInputElement;
 
   constructor() {
     this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
@@ -9,11 +12,30 @@ class ProjectInput {
 
     const importedNode = document.importNode(this.templateElement.content, true);
     this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.element.id = 'user-input' // cssのidを適用
+    
+    this.titleInputElement = this.element.querySelector("#title") as HTMLInputElement;
+    this.descriptionInputElement = this.element.querySelector("#description") as HTMLInputElement;
+    this.mandayInputElement = this.element.querySelector("#manday") as HTMLInputElement;
+
+    this.configure();
     this.attach();
   }
 
+  // イベントのオブジェクトを受け取る
+  private submitHandler(event: Event) {
+    // preventDefault このフォームからHTTPリクエストが行われないようにする
+    event.preventDefault();
+    console.log(this.titleInputElement.value);
+  }
+
+  // イベントリスナーの設定
+  private configure() {
+    this.element.addEventListener("submit", this.submitHandler)
+  }
+
+  // 要素を追加
   private attach() {
-    // 開始した直後にテンプレートからimportした要素を追加する
     this.hostElement.insertAdjacentElement('afterbegin', this.element)
   }
 }
