565ae4a31e8847ae7e2b471b2ec89a5f7c90a574
macotonacamura <65491593+macotonacamura@users.noreply.github.com>
2023-06-05 15:10:54 +0900

    ファイルとフォルダを整理

---
+++ src/components/project-list.ts
@@ -0,0 +1,82 @@
+/// <reference  path="base-component.ts"/>
+
+namespace App {
+  export class ProjectList
+    extends Component<HTMLDivElement, HTMLElement>
+    implements IDropTarget
+  {
+    assignedProjects: Project[]; // <section>
+
+    constructor(private type: "active" | "finished") {
+      super("project-list", "app", false, `${type}-projects`);
+      // 要素への参照を取得する作業
+      this.hostElement = document.getElementById("app")! as HTMLDivElement;
+      this.assignedProjects = [];
+
+      this.configure();
+      this.renderContent();
+    }
+
+    @autoBind
+    dragOverHandler(event: DragEvent): void {
+      if (event.dataTransfer && event.dataTransfer.types[0] === "text/plain") {
+        event.preventDefault();
+        const listEl = this.element.querySelector("ul");
+        if (listEl) {
+          listEl.classList.add("droppable");
+        }
+      }
+    }
+
+    @autoBind
+    dropHandler(event: DragEvent): void {
+      const prjId = event.dataTransfer!.getData("text/plain");
+      projectState.changeProjectStatus(
+        prjId,
+        this.type === "active" ? ProjectStatus.Active : ProjectStatus.Finished
+      );
+    }
+
+    @autoBind
+    dragLeaveHandler(_: DragEvent): void {
+      const listEl = this.element.querySelector("ul");
+      if (listEl) {
+        listEl.classList.remove("droppable");
+      }
+    }
+
+    configure() {
+      this.element.addEventListener("dragover", this.dragOverHandler)!;
+      this.element.addEventListener("drop", this.dropHandler)!;
+      this.element.addEventListener("dragleave", this.dragLeaveHandler)!;
+
+      projectState.addListener((projects: Project[]) => {
+        const relevantProjects = projects.filter((project) => {
+          if (this.type === "active") {
+            return project.status === ProjectStatus.Active;
+          }
+          return project.status === ProjectStatus.Finished;
+        });
+        this.assignedProjects = relevantProjects;
+        this.renderProjects();
+      });
+    }
+
+    renderContent() {
+      const listId = `${this.type}-projects-list`;
+      this.element.querySelector("ul")!.id = listId;
+      this.element.querySelector("h2")!.textContent =
+        this.type === "active" ? "実行中プロジェクト" : "完了プロジェクト";
+    }
+
+    private renderProjects() {
+      const listEl = document.getElementById(
+        `${this.type}-projects-list`
+      )! as HTMLUListElement;
+      listEl.innerHTML = "";
+      for (const prjItem of this.assignedProjects) {
+        new ProjectItem(listEl.id, prjItem);
+      }
+    }
+  }
+}
---
+++ src/state/project.ts
@@ -0,0 +1,58 @@
+namespace App {
+  type Listener<T> = (items: T[]) => void;
+  
+  class State<T> {
+    protected listeners: Listener<T>[] = []; // protected継承先のクラスからならアクセスできる（他はダメ）
+  
+    addListener(listenerFn: Listener<T>) {
+      this.listeners.push(listenerFn);
+    }
+  }
+  
+  export class ProjectState extends State<Project> {
+    private projects: Project[] = [];
+    private static instance: ProjectState; //インスタンスを保持するためのprop
+    private constructor() {
+      super();
+      // シングルトン
+    }
+  
+    static getInstance() {
+      if (this.instance) {
+        return this.instance;
+      }
+      this.instance = new ProjectState();
+      return this.instance;
+    }
+  
+    private updateListeners() {
+      for (const listenerFn of this.listeners) {
+        listenerFn(this.projects.slice());
+      }
+    }
+  
+    addProject(title: string, description: string, manday: number) {
+      const newProject = new Project(
+        Math.random().toString(),
+        title,
+        description,
+        manday,
+        ProjectStatus.Active
+      );
+      this.projects.push(newProject);
+      this.updateListeners();
+    }
+  
+    // D&Dでproject.stateを変える
+    changeProjectStatus(projectId: string, newStatus: ProjectStatus) {
+      // ここでListにアクセスする
+      const project = this.projects.find((p) => p.id === projectId);
+      if (project && project.status !== newStatus) {
+        project.status = newStatus;
+        this.updateListeners();
+      }
+    }
+  }
+  
+  export const projectState = ProjectState.getInstance(); // 他のクラスのようにnewするのではなく、getInstanceの処理で常にインスタンスが一つになるようにする
+}
